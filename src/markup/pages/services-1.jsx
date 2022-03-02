3550f16ea782267c56476da8d9f118291de50162
ricardowrk7 <jaymaynard84@gmail.com>
2022-03-01 21:31:38 -0500

    minor updates

--- src/markup/pages/services-1.jsx
+++ src/markup/pages/services-1.jsx
@@ -67,50 +67,50 @@ class Services1 extends Component {
                         <div class="row">
                             {/* <!-- Services Block One --> */}
                             <div class="service-block-one col-lg-4">
-                                <div class="inner-box">
+                                <div class="inner-box min-height">
                                     <div class="icon"><span class="flaticon-world"></span><span class="round-shape"></span></div>
                                     <h3>UI/UX Applications</h3>
-                                    <div class="text">Lorem ipsum dolor sit amet, consectet ur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</div>
+                                    <div class="text">User experience and user interface design, or UX/UI, is the art of designing digital products tailored to the way humans think about and interact with technology. Fresh has established a proven, scientific process for creating experiences that fulfill user needs and satisfy business goals. </div>
                                 </div>
                             </div>
                             {/* <!-- Services Block One --> */}
                             <div class="service-block-one col-lg-4">
-                                <div class="inner-box">
+                                <div class="inner-box min-height">
                                     <div class="icon"><span class="flaticon-money"></span><span class="round-shape"></span></div>
                                     <h3>Ecommerce</h3>
-                                    <div class="text">Lorem ipsum dolor sit amet, consectet ur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</div>
+                                    <div class="text">Many companies have seen gains from e-commerce—but fewer have achieved the transformational results they seek. By combining digital expertise and strategy, we help businesses set and realize a bold vision for their online channels.</div>
                                 </div>
                             </div>
                             {/* <!-- Services Block One --> */}
                             <div class="service-block-one col-lg-4">
-                                <div class="inner-box">
+                                <div class="inner-box min-height">
                                     <div class="icon"><span class="fab fa-cloudversify"></span><span class="round-shape"></span></div>
                                     <h3>Cloud Architecture</h3>
-                                    <div class="text">Lorem ipsum dolor sit amet, consectet ur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</div>
+                                    <div class="text">For clients who want to move their services into the cloud, thinking through the underlying technology and software architecture is essential. Fresh cloud architects help you evaluate the components and structure you need, including front-end platforms, back-end platforms, cloud delivery, and your network approach.</div>
                                 </div>
                             </div>
                             {/* <!-- Services Block One --> */}
                             <div class="service-block-one col-lg-4">
-                                <div class="inner-box">
+                                <div class="inner-box min-height">
                                     <div class="icon"><span class="fab fa-bitcoin"></span><span class="round-shape"></span></div>
                                     <h3>Blockchain</h3>
-                                    <div class="text">Lorem ipsum dolor sit amet, consectet ur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</div>
+                                    <div class="text">Leverage our blockchain consulting services to harness the potential of blockchain and gain valuable insights on your projects spanning, DeFi, NFTs, Web3, and Metaverse. From ideation to selecting the right protocol, our team turns your aspirations into practical application.</div>
                                 </div>
                             </div>
                             {/* <!-- Services Block One --> */}
                             <div class="service-block-one col-lg-4">
-                                <div class="inner-box">
+                                <div class="inner-box min-height">
                                     <div class="icon"><span class="fa fa-sitemap"></span><span class="round-shape"></span></div>
                                     <h3>Product Development Strategy</h3>
-                                    <div class="text">Lorem ipsum dolor sit amet, consectet ur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</div>
+                                    <div class="text">Product development strategy connects to corporate strategy in every possible way including: technology platforms, product roadmaps, distribution channels, and customer segments.</div>
                                 </div>
                             </div>
                             {/* <!-- Services Block One --> */}
                             <div class="service-block-one col-lg-4">
-                                <div class="inner-box">
+                                <div class="inner-box min-height">
                                     <div class="icon"><span class="flaticon-notebook"></span><span class="round-shape"></span></div>
                                     <h3>Responsive Interface</h3>
-                                    <div class="text">Lorem ipsum dolor sit amet, consectet ur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</div>
+                                    <div class="text">Every evolving business sees its user experience erode. Screens age. Priorities and preferences shift. Revive fatigued interfaces without disrupting operations — from incremental upgrades to total overhauls.</div>
                                 </div>
                             </div>
                         </div>
